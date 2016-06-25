d9d2d2e85227c9171254c3924b4963eb64067bd1
nicklin <qaz26688442@hotmail.com>
2016-06-25 17:17:24 +0800

    定義IRpository介面

---
+++ NickLibrary/Interfaces/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Repository.Interface
+{
+    internal interface IRepository<TEntity> : IDisposable
+        where TEntity : class
+    {
+        void Create(TEntity obj);
+
+        bool IsExists(int primaryKey);
+
+        TEntity GetByID(int primaryKey);
+
+        TEntity Get(Expression<Func<TEntity, bool>> predicate);
+
+        IQueryable<TEntity> GetAll();
+
+        IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        void Update(TEntity obj);
+
+        void Delete(TEntity obj);
+
+        void SaveChange();
+    }
+}
