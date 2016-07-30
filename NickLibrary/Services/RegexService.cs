1727b885cf40692242cf2b70255d81c3d49df74f
Nick <qaz26688442@hotmail.com>
2016-07-30 11:02:25 +0800

    新增測試專案與RegexService

---
+++ NickLibrary/Services/RegexService.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NickLibrary.Services
+{
+    public class RegexService
+    {
+        private Regex regex;
+
+        /// <summary>
+        /// 判斷串進來的字串是否為中文
+        /// </summary>
+        /// <param name="value">欲判斷得值</param>
+        /// <returns>是否為中文</returns>
+        public bool IsChinese(string value)
+        {
+            regex = new Regex("^[\u4e00-\u9fa5]");
+            foreach (var item in value)
+            {
+                if (!regex.IsMatch(item.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ NickLibraryTest/Services/RegexServiceTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NickLibrary.Services.Tests
+{
+    [TestClass()]
+    public class RegexServiceTests
+    {
+        [TestMethod()]
+        public void IsChinese_EnglishValue_False()
+        {
+            //Arrange
+            RegexService regexService = new RegexService();
+            bool expected = false;
+            //Acaual
+            bool actual = regexService.IsChinese("English");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsChinese_ChineseValue_Ture()
+        {
+            //Arrange
+            RegexService regexService = new RegexService();
+            bool expected = true;
+            //Acaual
+            bool actual = regexService.IsChinese("林菘彬");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsChinese_MixValue_Ture()
+        {
+            //Arrange
+            RegexService regexService = new RegexService();
+            bool expected = false;
+            //Acaual
+            bool actual = regexService.IsChinese("Lin林菘彬");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void IsChinese_NumberValue_Ture()
+        {
+            //Arrange
+            RegexService regexService = new RegexService();
+            bool expected = false;
+            //Acaual
+            bool actual = regexService.IsChinese("1234");
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
